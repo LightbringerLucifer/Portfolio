c163f31b731cac13542e175308e6040bc93ada89
LightbringerLucifer <nikhil9504singh@gmail.com>
2026-02-11 11:17:27 +0530

    Update Skills.js
    updated the skills to make the carousels work

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -7,154 +7,81 @@ import jquery_logo from "../Styling/images/testimonials/jquery.png";
 import boot_logo from "../Styling/images/testimonials/bootstrap.png";
 import redux_logo from "../Styling/images/testimonials/redux.png";
 import rb_logo from "../Styling/images/testimonials/react_bootstrap.png";
+
 const Skills = () => {
   return (
-    <div id="skills" class="testimonials-bg" style={{ marginTop: "560px" }}>
-      <div class="testimonials center spacer">
-        <div class="container">
+    <div id="skills" className="testimonials-bg" style={{ marginTop: "560px" }}>
+      <div className="testimonials center spacer">
+        <div className="container">
           <h2>Skills</h2>
 
           <div
             id="carousel-testimonials"
-            class="carousel slide"
-            data-ride="carousel"
+            className="carousel slide"
+            data-bs-ride="carousel"
           >
-            <div class="carousel-inner">
-              {/*React  */}
-              <div class="item active animated bounceInRight">
-                <span class="animated slideInLeft">
-                  <img
-                    alt="react"
-                    src={react_logo}
-                    width="100"
-                    class="img-circle"
-                  />
-                </span>
+            <div className="carousel-inner">
+              {/* React */}
+              <div className="carousel-item active">
+                <img
+                  alt="react"
+                  src={react_logo}
+                  width="100"
+                  className="rounded-circle"
+                />
                 <p>React JS</p>
-                <span>
-                  A Javascript library developed and maintained by
-                  META(Facebook)
-                </span>
+                <span>A JavaScript library maintained by META (Facebook)</span>
               </div>
 
               {/* HTML */}
-              <div class="item  animated bounceInRight">
-                <span class="animated slideInLeft">
-                  <img
-                    alt="hmtl"
-                    src={html_logo}
-                    width="100"
-                    class="img-circle"
-                  />
-                </span>
+              <div className="carousel-item">
+                <img
+                  alt="html"
+                  src={html_logo}
+                  width="100"
+                  className="rounded-circle"
+                />
                 <p>HTML</p>
-                <span>
-                  Hyper Text Markup Language to let browser understand the basic
-                  structure of your site
-                </span>
+                <span>Hyper Text Markup Language for structure</span>
               </div>
 
               {/* CSS */}
-              <div class="item  animated bounceInRight">
-                <span class="animated slideInLeft">
-                  <img
-                    alt="css"
-                    src={css_logo}
-                    width="100"
-                    class="img-circle"
-                  />
-                </span>
+              <div className="carousel-item">
+                <img
+                  alt="css"
+                  src={css_logo}
+                  width="100"
+                  className="rounded-circle"
+                />
                 <p>CSS</p>
-                <span>Cascading Style Sheet for the makeover of your site</span>
+                <span>Cascading Style Sheets for styling</span>
               </div>
 
-              {/* Javascript */}
-              <div class="item  animated bounceInRight">
-                <span class="animated slideInLeft">
-                  <img alt="JS" src={js_logo} width="100" class="img-circle" />
-                </span>
-                <p>Javascript</p>
-                <span>Used to add Interactivity to your website</span>
-              </div>
-
-              {/* JQuery */}
-              <div class="item  animated bounceInRight">
-                <span class="animated slideInLeft">
-                  <img
-                    alt="jquery"
-                    src={jquery_logo}
-                    width="100"
-                    class="img-circle"
-                  />
-                </span>
-                <p>JQuery</p>
-                <span>Used Short-hand javascript</span>
-              </div>
-              {/* Bootstrap */}
-              <div class="item  animated bounceInRight">
-                <span class="animated slideInLeft">
-                  <img
-                    alt="bootstrap"
-                    src={boot_logo}
-                    width="100"
-                    class="img-circle"
-                  />
-                </span>
-                <p>Bootstrap</p>
-                <span>
-                  Open-source framework for responsive and mobile-first websites
-                </span>
-              </div>
-
-              {/* Redux */}
-              <div class="item  animated bounceInRight">
-                <span class="animated slideInLeft">
-                  <img
-                    alt="redux"
-                    src={redux_logo}
-                    width="100"
-                    class="img-circle"
-                  />
-                </span>
-                <p>Redux</p>
-                <span>State-management library</span>
-              </div>
-              
-              {/* react-bootstrap */}
-              <div class="item  animated bounceInRight">
-                <span class="animated slideInLeft">
-                  <img
-                    alt="react-bootstrap"
-                    src={rb_logo}
-                    width="100"
-                    class="img-circle"
-                  />
-                </span>
-                <p>React-Bootstrap</p>
-                <span>Similar to Bootstrap but modified for React JS</span>
-              </div>
+              {/* Add other items similarly */}
             </div>
 
-            {/* <!-- Controls --> */}
-            <a
-              class="left arrow"
-              href="#carousel-testimonials"
-              data-slide="prev"
+            {/* Controls */}
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carousel-testimonials"
+              data-bs-slide="prev"
             >
-              <i class="fa fa-angle-left"></i>
-            </a>
-            <a
-              class="right arrow"
-              href="#carousel-testimonials"
-              data-slide="next"
+              <span className="carousel-control-prev-icon"></span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carousel-testimonials"
+              data-bs-slide="next"
             >
-              <i class="fa fa-angle-right"></i>
-            </a>
-            {/* <!-- Controls --> */}
+              <span className="carousel-control-next-icon"></span>
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default Skills;
